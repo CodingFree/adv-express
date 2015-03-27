810ba5cd6ced176a55a2a56653785dda5a52e4eb
Adrian <madrid.crespo@gmail.com>
2015-03-27 23:10:19 +0100

    Fixed sizes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#content').html("<br/><br/><center><img src='images/spinner.gif' width='50px' height='50px'/></center>");
+  $('#content').html("<br/><br/><center><img class='spinner' src='images/spinner.gif' width='50px' height='50px'/></center>");
   var url = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=20&q=http://ano.lolcathost.org/rss-sfw.xml';
 
   $.ajax({
@@ -13,8 +13,8 @@ $(document).ready(function() {
     success: function(data) {
       console.log(data);
       var printa = function(e) {
-        $('#content').append("<div class='item fit'><dl><dt class='title round info label fit'>" + e.title.replace("y su ADV", "") + "</dt><dd class='fit txt'>" + e.content.split("<br><br>")[0] + "</dd></dl></div><br/>");
-
+        $('#content').append("<div class='item fit'><dl><dt class='title round info label fit'>" + e.title.substring(0, 17) + "</dt><dd class='fit txt'>" + e.content.split("<br><br>")[0] + "</dd></dl></div><br/>");
+        console.log(e.content.split("<br><br>")[0]);
       }
 
       $('#content').html("");
