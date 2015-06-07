aad2aa0d68477babb5ecfa1ac8df3579e47fe590
CodingFree <madrid.crespo@gmail.com>
2015-06-07 21:11:37 +0200

    Trollface again

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var translate = navigator.mozL10n.get;
+
   $('#content').html("<br/><br/><center><img class='spinner' src='images/spinner.gif' width='50px' height='50px'/></center>");
   var url = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=20&q=http://ano.lolcathost.org/rss-sfw.xml';
 
@@ -11,18 +13,24 @@ $(document).ready(function() {
     },
     url: url,
     success: function(data) {
-      console.log(data);
+  //console.log(data);
       var printa = function(e) {
         $('#content').append("<div class='item fit'><dl><dt class='title round info label fit'>" + e.title.substring(0, 17) + "</dt><dd class='fit txt'>" + e.content.split("<br><br>")[0] + "</dd></dl></div><br/>");
-        console.log(e.content.split("<br><br>")[0]);
+      //console.log(e.content.split("<br><br>")[0]);
       }
 
       $('#content').html("");
       data.responseData.feed.entries.map(printa);
     },
     error: function() {
-      alert("Sorry, I can't get the feed");
-    }
+      $('#content').empty();
+      $('#content').append("<div data-l10n-id=\"error_message\">"+navigator.mozL10n.get("error_message")+"</div></br>");
+      $('#content').append("<button data-l10n-id='reload' id=\"reload\" label'>"+navigator.mozL10n.get("reload")+"</button>");
+      $('#reload').click(function(){
+        location.reload();
+      });
+    },
+    timeout: 3000
   });
 
 });
